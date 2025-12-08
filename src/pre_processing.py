8b3563cf52d095dddcd71e844a89166d547620a9
Nir David Duani <nirdavid.duani@post.runi.ac.il>
2025-12-08 20:10:11 +0200

    implemment pre_process

--- src/pre_processing.py
+++ src/pre_processing.py
@@ -1,9 +1,13 @@
-from pytube import YouTube
+from moviepy import VideoFileClip
+clip = VideoFileClip("../data/raw/full_video_fixed.mp4")
 
-url = "https://www.youtube.com/watch?v=GgXAs7UVpig"
+start_time = 7*60 + 48   # 468 שניות
+end_time   = 12*60 + 50  # 770 שניות
 
-yt = YouTube(url)
-stream = yt.streams.filter(file_extension="mp4").get_highest_resolution()
-stream.download("data/raw", filename="full_video.mp4")
+subclip = clip.subclipped(start_time, end_time)
 
-print("Downloaded!")
+# --- שלב 4: שמירה של הקטע לתיקייה processed ---
+output_path = "../data/processed/highway_clip.mp4"
+subclip.write_videofile(output_path, codec="libx264", audio_codec="aac")
+
+print("Saved trimmed clip to:", output_path)
